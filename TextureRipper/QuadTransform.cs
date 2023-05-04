f84564099fe81d89351ce0df9de3be0d15810da0
Marco <marcosteam7@gmail.com>
2023-05-04 02:30:12 -0400

    gui setup

---
+++ TextureRipper/QuadTransform.cs
@@ -0,0 +1,13 @@
+﻿namespace TextureRipper;
+
+public class QuadTransform
+{
+    private double[] src;
+    private double[] dst;
+
+    public QuadTransform(double[] src, double[] dst)
+    {
+        this.src = src;
+        this.dst = dst;
+    }
+}
