8f72b29b9fa32706fae2d161fd00848134dd4ee6
Marco <marcosteam7@gmail.com>
2023-06-03 12:56:06 -0400

    Update MainWindow.xaml.cs

--- TextureRipper/MainWindow.xaml.cs
+++ TextureRipper/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
